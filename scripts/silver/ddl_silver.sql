6b33bafe950c389fbac6234d7defa6355f757629
sushrut-code <sushrut452@gmail.com>
2025-06-14 20:22:12 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -9,82 +9,160 @@ Script Purpose:
 ===============================================================================
 */
 
-IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_cust_info;
-GO
+-- CRM Customer Info
+DROP TABLE IF EXISTS silver.crm_cust_info;
 
 CREATE TABLE silver.crm_cust_info (
     cst_id              INT,
-    cst_key             NVARCHAR(50),
-    cst_firstname       NVARCHAR(50),
-    cst_lastname        NVARCHAR(50),
-    cst_marital_status  NVARCHAR(50),
-    cst_gndr            NVARCHAR(50),
-    cst_create_date     DATE
+    cst_key             VARCHAR(50),
+    cst_firstname       VARCHAR(50),
+    cst_lastname        VARCHAR(50),
+    cst_marital_status  VARCHAR(50),
+    cst_gndr            VARCHAR(50),
+    cst_create_date     DATE,
+    dwh_create_date     DATE
 );
-GO
 
-IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_prd_info;
-GO
+DELIMITER //
+
+CREATE TRIGGER trg_set_dwh_date_crm_cust_info
+BEFORE INSERT ON silver.crm_cust_info
+FOR EACH ROW
+BEGIN
+    IF NEW.dwh_create_date IS NULL THEN
+        SET NEW.dwh_create_date = CURDATE();
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+-- CRM Product Info
+DROP TABLE IF EXISTS silver.crm_prd_info;
 
 CREATE TABLE silver.crm_prd_info (
     prd_id       INT,
-    prd_key      NVARCHAR(50),
-    prd_nm       NVARCHAR(50),
+    prd_key      VARCHAR(50),
+    prd_nm       VARCHAR(50),
     prd_cost     INT,
-    prd_line     NVARCHAR(50),
+    prd_line     VARCHAR(50),
     prd_start_dt DATETIME,
-    prd_end_dt   DATETIME
+    prd_end_dt   DATETIME,
+    dwh_create_date DATE
 );
-GO
 
-IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
-    DROP TABLE silver.crm_sales_details;
-GO
+DELIMITER //
+
+CREATE TRIGGER trg_set_dwh_date_crm_prd_info
+BEFORE INSERT ON silver.crm_prd_info
+FOR EACH ROW
+BEGIN
+    IF NEW.dwh_create_date IS NULL THEN
+        SET NEW.dwh_create_date = CURDATE();
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+-- CRM Sales Details
+DROP TABLE IF EXISTS silver.crm_sales_details;
 
 CREATE TABLE silver.crm_sales_details (
-    sls_ord_num  NVARCHAR(50),
-    sls_prd_key  NVARCHAR(50),
+    sls_ord_num  VARCHAR(50),
+    sls_prd_key  VARCHAR(50),
     sls_cust_id  INT,
     sls_order_dt INT,
     sls_ship_dt  INT,
     sls_due_dt   INT,
     sls_sales    INT,
     sls_quantity INT,
-    sls_price    INT
+    sls_price    INT,
+    dwh_create_date DATE
 );
-GO
 
-IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
-    DROP TABLE silver.erp_loc_a101;
-GO
+DELIMITER //
+
+CREATE TRIGGER trg_set_dwh_date_crm_sales_details
+BEFORE INSERT ON silver.crm_sales_details
+FOR EACH ROW
+BEGIN
+    IF NEW.dwh_create_date IS NULL THEN
+        SET NEW.dwh_create_date = CURDATE();
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+-- ERP Location A101
+DROP TABLE IF EXISTS silver.erp_loc_a101;
 
 CREATE TABLE silver.erp_loc_a101 (
-    cid    NVARCHAR(50),
-    cntry  NVARCHAR(50)
+    cid    VARCHAR(50),
+    cntry  VARCHAR(50),
+    dwh_create_date DATE
 );
-GO
 
-IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
-    DROP TABLE silver.erp_cust_az12;
-GO
+DELIMITER //
+
+CREATE TRIGGER trg_set_dwh_date_erp_loc_a101
+BEFORE INSERT ON silver.erp_loc_a101
+FOR EACH ROW
+BEGIN
+    IF NEW.dwh_create_date IS NULL THEN
+        SET NEW.dwh_create_date = CURDATE();
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+-- ERP Customer AZ12
+DROP TABLE IF EXISTS silver.erp_cust_az12;
 
 CREATE TABLE silver.erp_cust_az12 (
-    cid    NVARCHAR(50),
+    cid    VARCHAR(50),
     bdate  DATE,
-    gen    NVARCHAR(50)
+    gen    VARCHAR(50),
+    dwh_create_date DATE
 );
-GO
 
-IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
-    DROP TABLE silver.erp_px_cat_g1v2;
-GO
+DELIMITER //
+
+CREATE TRIGGER trg_set_dwh_date_erp_cust_az12
+BEFORE INSERT ON silver.erp_cust_az12
+FOR EACH ROW
+BEGIN
+    IF NEW.dwh_create_date IS NULL THEN
+        SET NEW.dwh_create_date = CURDATE();
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+-- ERP Price Category G1V2
+DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
 
 CREATE TABLE silver.erp_px_cat_g1v2 (
-    id           NVARCHAR(50),
-    cat          NVARCHAR(50),
-    subcat       NVARCHAR(50),
-    maintenance  NVARCHAR(50)
+    id           VARCHAR(50),
+    cat          VARCHAR(50),
+    subcat       VARCHAR(50),
+    maintenance  VARCHAR(50),
+    dwh_create_date DATE
 );
-GO
+
+DELIMITER //
+
+CREATE TRIGGER trg_set_dwh_date_erp_px_cat_g1v2
+BEFORE INSERT ON silver.erp_px_cat_g1v2
+FOR EACH ROW
+BEGIN
+    IF NEW.dwh_create_date IS NULL THEN
+        SET NEW.dwh_create_date = CURDATE();
+    END IF;
+END;
+//
+
+DELIMITER ;
